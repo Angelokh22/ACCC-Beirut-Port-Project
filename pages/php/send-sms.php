1863600d8c2c50a3e7d8af94fec7f40952c791e5
BadranChris <chrisbadran03@gmail.com>
2024-06-10 18:46:01 +0000

    modified:   pages/php/send-sms.php

--- pages/php/send-sms.php
+++ pages/php/send-sms.php
@@ -12,7 +12,7 @@
     function send_sms($code, $to){
 
         $sid = "ACf837d1407e959d322c92af63df7a916c";
-        $token = "7226513140fa667df42ffd42cce34e08";
+        $token = "be9656dc6dc1bcf9765d05c051f10a1a";
 
         $client = new Services_Twilio($sid, $token);
         $message = $client->account->messages->sendMessage(
@@ -21,21 +21,5 @@
         "Please use this code to verify: $code"
         );
 
-        // $client = new Twilio\Rest\Client($sid, $token);
-
-        // // Use the Client to make requests to the Twilio REST API
-        // $client->messages->create(
-        //     // The number you'd like to send the message to
-        //     "$to",
-        //     [
-        //         // A Twilio phone number you purchased at https://console.twilio.com
-        //         'from' => '+14239272158',
-        //         // The body of the text message you'd like to send
-        //         'body' => "Please use this code to verify: $code"
-        //     ]
-        // );
     }
-
-    // send_sms("1234", "+9613875684");
-
 ?>
