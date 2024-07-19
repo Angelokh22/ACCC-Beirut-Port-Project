a469c4ee174639ae2b19dc4c8cf302e29b050378
Angelokh22 <88833779+Angelokh22@users.noreply.github.com>
2024-07-19 11:50:39 +0000

    new file:   pages/php/add_contact.php

---
+++ pages/php/add_contact.php
@@ -0,0 +1,34 @@
+<?php
+
+    include "tools.php";
+
+    if(isset($_POST['fname']) && $_POST['fname'] != ""
+       &&
+       isset($_POST['lname']) && $_POST['lname'] != ""
+       &&
+       isset($_POST['email']) && $_POST['email'] != ""
+       &&
+       isset($_POST['phone']) && $_POST['phone'] != "" 
+       &&
+       isset($_POST['message']) && $_POST['message'] != ""  
+    )
+    {
+
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+
+
+        $query = "INSERT INTO Contacts (`contactFName`, `contactLName`, `contactEmail`, `contactPhone`, `contactMessage`)
+                  VALUES (:cfname, :clname, :cemail, :cphone, :cmessage)";
+
+        send_query($query, false, false, ["cfname" => $fname, "clname" => $lname, "cemail" => $email, "cphone" => $phone, "cmessage" => $message]);
+
+        return json_encode(['success' => true, 'message' => 'Message sent successfully!']);
+
+    }
+    return json_encode(['success' => false, 'error' => 'Some parameters are required!']);
+
+?>
